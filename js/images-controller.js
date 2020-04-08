ff48753253d35ade4421e8f4a1f1ef65b22b3303
Matan Shaviro <matanshaviro@matans-mbp-2.mynet>
2020-04-09 00:16:11 +0300

    clean up the code

--- js/images-controller.js
+++ js/images-controller.js
@@ -6,27 +6,27 @@ $('.modal').modal();
 var gIsMarked = false;
 
 function onInit() {
-  createImages()
-  let images = getImages();
-  renderImages(images)
+    createImages()
+    let images = getImages();
+    renderImages(images)
 }
 
 function renderImages(images) {
-  let strHTML = images.map(image => `<div data-target="modal" onclick="onImageClick(${image.id})"><img src="img/${image.id}.jpg"/></div>`)
-  $('.images').html(strHTML.join(''))
+    let strHTML = images.map(image => `<div data-target="modal" onclick="onImageClick(${image.id})"><img src="img/${image.id}.jpg"/></div>`)
+    $('.images').html(strHTML.join(''))
 }
 
 function onRenderByKeyWord(keyWord) {
-  let images = getImages()
-  let filteredImages = []
-  for (let i = 0; i < images.length; i++) {
-    let currentImage = getImageByKeyWord(images[i], keyWord)
-    if (currentImage !== null) filteredImages.push(currentImage)
-  }
-  renderImages(filteredImages)
+    let images = getImages()
+    let filteredImages = []
+    for (let i = 0; i < images.length; i++) {
+        let currentImage = getImageByKeyWord(images[i], keyWord)
+        if (currentImage !== null) filteredImages.push(currentImage)
+    }
+    renderImages(filteredImages)
 }
 
 function onSubmitMessage(event) {
-  event.preventDefault()
-  Materialize.toast('Messege Sent', 4000, 'rounded')
+    event.preventDefault()
+    Materialize.toast('Messege Sent', 4000, 'rounded')
 }
